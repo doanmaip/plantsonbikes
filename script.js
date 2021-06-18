a087440a8fca936f8166449af742966a3a29aa25
Mai <mai.phuong.doan@hs-furtwangen.de>
2021-06-18 22:31:23 +0200

    counter

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 import AOS from 'aos';
 
 AOS.init();
+
+const counters = document.querySelectorAll('.value');
+const speed = 200;
+
+counters.forEach(counter => {
+    const animate = () => {
+        const value = +counter.getAttribute('akhi');
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if (data < value) {
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 1);
+        } else {
+            counter.innerText = value;
+        }
+
+    }
+
+    animate();
+});
